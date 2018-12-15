0509f06659b5925a21118c085ca02e96b403e620
Michael Cornillon <contact@dwaps.fr>
2018-12-15 15:30:25 +0100

    Déclaration des classes de mouvement du cube

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 (function() {
     const controlAreas = document.querySelectorAll('.control-area span')
     const cube = document.querySelector('.cube')
+    
+    const CUBE_TO_TOP = 'cubeToTop'
+    const CUBE_TO_RIGHT = 'cubeToRight'
+    const CUBE_TO_BOTTOM = 'cubeToBottom'
+    const CUBE_TO_LEFT = 'cubeToLeft'
+
+    const moveClasses = [CUBE_TO_TOP, CUBE_TO_RIGHT, CUBE_TO_BOTTOM, CUBE_TO_LEFT]
 })();
