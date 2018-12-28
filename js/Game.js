c4cb83d3f110aa5b628310acf99f5dbc76ee8be9
Michael Cornillon <contact@dwaps.fr>
2018-12-28 10:54:27 +0100

    Création de la scène : les arbres

--- js/Game.js
+++ js/Game.js
@@ -40,12 +40,26 @@ define(function () {
           ],
         },
       },
+      trees: {
+        trunk: {
+          color: `rgba(100, 0, 20, ${alpha})`,
+        },
+        foliage: {
+          color: [
+            `rgba(0, 140, 25, ${alpha})`,
+            `rgba(0, 110, 25, ${alpha})`,
+            `rgba(0, 80, 25, ${alpha})`,
+            `rgba(0, 50, 25, ${alpha})`,
+          ],
+        },
+      },
     },
   }
 
   function drawScene() {
     drawSkyAndOcean.call(this)
     drawMounts.call(this)
+    drawTrees.call(this)
   }
 
   function drawSkyAndOcean() {
@@ -112,6 +126,59 @@ define(function () {
       context.lineTo(x + 400, 400)
       context.fill()
     }
+  }
+
+  function drawTrees() {
+    const { context, scene } = this
+
+    // Left tree
+    context.fillStyle = scene.trees.trunk.color
+    context.fillRect(100, 200, 20, 150)
+
+    context.beginPath()
+    context.fillStyle = scene.trees.foliage.color[0]
+    context.arc(140, 180, 40, 0, 2 * Math.PI)
+    context.fill()
+
+    context.beginPath()
+    context.fillStyle = scene.trees.foliage.color[1]
+    context.arc(80, 190, 30, 0, 2 * Math.PI)
+    context.fill()
+
+    context.beginPath()
+    context.fillStyle = scene.trees.foliage.color[2]
+    context.arc(110, 200, 50, 0, 2 * Math.PI)
+    context.fill()
+
+    context.beginPath()
+    context.fillStyle = scene.trees.foliage.color[3]
+    context.arc(120, 230, 30, 0, 2 * Math.PI)
+    context.fill()
+
+    // Right tree
+    context.fillStyle = scene.trees.trunk.color
+    context.fillRect(900, 200, 20, 150)
+
+    context.beginPath()
+    context.fillStyle = scene.trees.foliage.color[3]
+    context.moveTo(850, 300)
+    context.lineTo(910, 200)
+    context.lineTo(970, 300)
+    context.fill()
+
+    context.beginPath()
+    context.fillStyle = scene.trees.foliage.color[2]
+    context.moveTo(850, 250)
+    context.lineTo(910, 150)
+    context.lineTo(970, 250)
+    context.fill()
+
+    context.beginPath()
+    context.fillStyle = scene.trees.foliage.color[1]
+    context.moveTo(850, 200)
+    context.lineTo(910, 100)
+    context.lineTo(970, 200)
+    context.fill()
   }
 
   return Game
