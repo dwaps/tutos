6a1ac93b8ef4b76efd3a427729972cb058faa0d1
Michael Cornillon <contact@dwaps.fr>
2019-01-02 13:28:17 +0100

    Simple TimePicker

--- MainActivity.java
+++ MainActivity.java
@@ -3,56 +3,32 @@ package fr.dwaps.tutos;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.RelativeLayout;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
 
-    private TextView mTitleTV;
-    private RelativeLayout mContainerRL;
+public class MainActivity extends AppCompatActivity implements TimePicker.OnTimeChangedListener {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  private TextView mTitleTV;
+  private TimePicker mTimerTP;
 
-        mTitleTV = findViewById(R.id.activity_main_title_tv);
-        mContainerRL = findViewById(R.id.activity_main_container_rl);
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    mTitleTV = findViewById(R.id.activity_main_title_tv);
+    mTimerTP = findViewById(R.id.activity_main_timer_tp);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int color = 0;
-        String text = "";
+    mTimerTP.setIs24HourView(true);
+    mTimerTP.setOnTimeChangedListener(this);
+  }
 
-        switch (item.getItemId()) {
-            case R.id.home:
-                color = Color.GREEN;
-                text = getResources().getString(R.string.home_str);
-                break;
-            case R.id.about:
-                color = Color.MAGENTA;
-                text = getResources().getString(R.string.about_str);
-                break;
-            case R.id.contact:
-                color = Color.CYAN;
-                text = getResources().getString(R.string.contact_str);
-                break;
-        }
-
-        if (color != 0 && !text.isEmpty()) {
-            mContainerRL.setBackgroundColor(color);
-            mTitleTV.setText(text);
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+  @Override
+  public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+    mTitleTV.setVisibility(View.VISIBLE);
+    mTitleTV.setText(String.format(Locale.FRANCE, "Réveil réglé à %d:%d", hourOfDay, minute));
+  }
 }
