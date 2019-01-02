1e91489ba99f82164698ce3e50b324f8bc1b3dc7
Michael Cornillon <contact@dwaps.fr>
2019-01-02 19:34:44 +0100

    Simple AlertDialog

--- java/MainActivity.java
+++ java/MainActivity.java
@@ -1,39 +1,70 @@
 package fr.dwaps.tutos;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private View toastLayout;
-
-    private String mDefaultToastStr;
-    private String mCustomToastStr;
+    private boolean touchOnce = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mDefaultToastStr = getResources().getString(R.string.default_toast_message);
-        mCustomToastStr = getResources().getString(R.string.custom_toast_message);
-
-        toastLayout = getLayoutInflater().inflate(R.layout.toast, (ViewGroup) findViewById(R.id.toast));
     }
 
-    public void showDefaultToast(View view) {
-        Toast.makeText(this, mDefaultToastStr, Toast.LENGTH_SHORT).show();
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (touchOnce) {
+            touchOnce = false;
+            showAlertDialog(null);
+        }
+        return super.onTouchEvent(event);
     }
 
-    public void showCustomToast(View view) {
-        Toast toast = Toast.makeText(this, mCustomToastStr, Toast.LENGTH_SHORT);
-        toast.setGravity(Gravity.CENTER, 0, 0);
-        toast.setView(toastLayout);
-        toast.show();
+    private void showAlertDialog(String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(false);
+
+        if (message == null) {
+            builder.setMessage(Html.fromHtml(getString(R.string.alertdialog_message)));
+
+            builder.setNegativeButton("Tais-toi !", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                    touchOnce = true;
+                }
+            });
+
+            builder.setPositiveButton("Oui tu es gentille", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    showAlertDialog(getString(R.string.alertdialog_thanks));
+                }
+            });
+        }
+        else {
+            builder.setCancelable(true);
+            builder.setMessage(message);
+
+            builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                @Override
+                public void onDismiss(DialogInterface dialog) {
+                    touchOnce = true;
+                }
+            });
+        }
+
+        builder.create().show();
     }
 }
