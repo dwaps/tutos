b7b31e5d2a697be963ed8493058662b8e2add227
Michael Cornillon <contact@dwaps.fr>
2019-01-02 14:13:22 +0100

    Simple DatePicker

--- MainActivity.java
+++ MainActivity.java
@@ -1,34 +1,45 @@
 package fr.dwaps.tutos;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.TextView;
-import android.widget.TimePicker;
 
+import java.time.LocalDate;
+import java.time.format.TextStyle;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity implements TimePicker.OnTimeChangedListener {
+public class MainActivity extends AppCompatActivity implements DatePicker.OnDateChangedListener {
 
-  private TextView mTitleTV;
-  private TimePicker mTimerTP;
+    private TextView mTitleTV;
+    private DatePicker mCalendarDP;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    mTitleTV = findViewById(R.id.activity_main_title_tv);
-    mTimerTP = findViewById(R.id.activity_main_timer_tp);
+        mTitleTV = findViewById(R.id.activity_main_title_tv);
+        mCalendarDP = findViewById(R.id.activity_main_calendar_dp);
 
-    mTimerTP.setIs24HourView(true);
-    mTimerTP.setOnTimeChangedListener(this);
-  }
+        LocalDate now = LocalDate.now();
+        mCalendarDP.init(
+            now.getYear(),
+            now.getMonth().getValue(),
+            now.getDayOfMonth(),
+            this
+        );
+    }
 
-  @Override
-  public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
-    mTitleTV.setVisibility(View.VISIBLE);
-    mTitleTV.setText(String.format(Locale.FRANCE, "Réveil réglé à %d:%d", hourOfDay, minute));
-  }
+    @Override
+    public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        LocalDate date = LocalDate.of(year, monthOfYear+1, dayOfMonth);
+        String month = date.getMonth().getDisplayName(TextStyle.FULL, Locale.FRANCE);
+
+        mTitleTV.setVisibility(View.VISIBLE);
+        mTitleTV.setText(
+            String.format(Locale.FRANCE, "Date du rdv : %d %s %d", dayOfMonth, month, year)
+        );
+    }
 }
